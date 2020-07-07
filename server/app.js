c07a7f12979f3701ef1d5a58cd2c79ac2e3f8f92
zoomkoding <zoomkoding@gmail.com>
2020-07-07 17:00:12 +0900

    etc: Pug 설정

--- server/app.js
+++ server/app.js
@@ -10,14 +10,14 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, '../client/src/pages'));
+app.set('views', path.join(__dirname, '../client/src/views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../client/src')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
