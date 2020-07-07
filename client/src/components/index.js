dc4c00d676221602b69bf33515302a1fb92df590
zoomkoding <gur5381@gmail.com>
2020-07-08 03:08:37 +0900

    update: Atomic Design Pattern에 맞게  구조화함

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,1 +1,4 @@
-console.log('hello');
+import Button from '/components/atoms/button/index.js';
+
+document.body.appendChild(Button('default', 'default', '배민사장님광장 로그인'))
+document.body.appendChild(Button('default', 'inline', '인증받기'))
