82757107405f9c9fe3ec3828f98e7063a1dc625d
zoomkoding <zoomkoding@gmail.com>
2020-07-07 18:15:18 +0900

    bug: User Controller 모델참조 수정

--- server/controllers/user_controller.js
+++ server/controllers/user_controller.js
@@ -1,4 +1,4 @@
-const Users = require('../models/curation');
+const Users = require('../models/users');
 
 exports.createUser = (req, res) => res.send(Users.create(req.body));
 exports.getUserByIdPw = (req, res) => res.send(Users.getUserByIdPw(req.body))
