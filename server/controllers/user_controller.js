ef21f164c6e63a96cab66219c8f6a3f4776c88da
zoomkoding <zoomkoding@gmail.com>
2020-07-07 20:04:42 +0900

    update: Users 테이블 내부 구조 정의, create, getByIdPw 함수 구현

--- server/controllers/user_controller.js
+++ server/controllers/user_controller.js
@@ -1,4 +1,12 @@
 const Users = require('../models/users');
 
-exports.createUser = (req, res) => res.send(Users.create(req.body));
-exports.getUserByIdPw = (req, res) => res.send(Users.getUserByIdPw(req.body))
+exports.createUser = (req, res) => {
+    const user = Users.create(req.body);
+    res.send(user);
+}
+
+exports.getUserByIdPw = (req, res) => {
+    const user = Users.getByIdPw(req.body);
+    if(user) res.send(user);
+    else res.status(404).send('user not found');
+}
--- server/models/users.js
+++ server/models/users.js
@@ -1,8 +1,23 @@
 class Users { 
-    users = [];
-    static getUserByIdPw = ({id, pw}) => this.users.find(user => user.id === id && user.pw === pw);
-    static create = (user) => this.users.push(user);
-    static update =  (user) => this.users[this.users.findIndex(user.id)] = user;
-    static delete = (user) => this.users[this.users.findIndex(user.id)] = null;
+    static users = [];
+
+    static getByIdPw = function ({id, pw}) {
+        return this.users.find(user => user.id === id && user.pw === pw);
+    }
+
+    static create = function (user) {
+        this.users.push(user);
+        return user;
+    }
+
+    static update = function (user) {
+        const id = this.users.findIndex(user.id);
+        this.users[id] = user;
+    } 
+
+    static delete = function (user) {
+        const id = this.users.findIndex(user.id);
+        if(id) this.users[id] = null;
+    }
 }
-exports = Users;
+module.exports = Users;
