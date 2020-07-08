cebd88b71081bdbb9619a5d191626f3b91b5afd0
zoomkoding <zoomkoding@gmail.com>
2020-07-08 18:40:51 +0900

    feature: button atom 구현 #16

--- client/src/components/atoms/button/index.js
+++ client/src/components/atoms/button/index.js
@@ -1,32 +1,9 @@
 import { button } from '/utils/elements.js';
 
-const getStyles = (styleType = 'default') => {
-    return styleType === 'default' ? null : ({
-        '-webkit-tap-highlight-color' : 'rgba(255, 255, 255, 0)',
-        'border-radius': 0,
-        'display': 'inline-block',
-        'padding': '14px 0 12px',
-        'text-align': 'center',
-        'font-weight': 'bold',
-        'font-size': '16px',
-        'line-height': '22px',
-        'border': '1px solid',
-        'background': '#fff',
-        'width': '98px',
-        'border-color': '#ccc',
-        'color': '#bbc0cd',
-        'position': 'absolute',
-        'top': 0,
-        'right': 0,
-        'left': '227px',
-    });
-    
-}
-
 const Button = (
     className, 
-    styleType,
     buttonText,
-) => button({ className, style : getStyles(styleType) }, buttonText );
+    onclick,
+) => button({ className, onclick }, buttonText);
 
 export default Button;
