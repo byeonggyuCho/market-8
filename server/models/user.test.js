5078dc4feac90bb3671ca2a0c778e1b370257188
myungwoo-Y <auddn6676@naver.com>
2020-07-08 18:12:30 +0900

    feature: login api 구현 및 test code 작성

--- server/models/user.test.js
+++ server/models/user.test.js
@@ -1,25 +1,99 @@
 const assert = require('assert'); 
+const request = require('supertest');
 const Users = require("./users");
+const app = require('../app');
 
-describe('findIdInUsers Test', function () {
-  Users.init();
-  it('find exsited user', function () {
-    assert.strictEqual(Users.isIdInDB('auddn6676'), true);
+Users.init();
+
+describe('Sample Test', () => {
+  it('should test that true === true', () => {
+    expect(true).toBe(true)
+  })
+})
+
+describe('idIdInDB Test', () => {
+  it('find exsited user', () => {
+    console.log(Users.users);
+    expect(Users.isIdInDB('auddn6676')).toBe(true);
   });
   it('find not existed user', function () {
-    assert.strictEqual(Users.isIdInDB('auddn667'), false);
+    expect(Users.isIdInDB('auddn667')).toBe(false);
   });
   it('empty string', function () {
-    assert.strictEqual(Users.isIdInDB(''), false);
+    expect(Users.isIdInDB('')).toBe(false);
   });
   it('param is number', function () {
-    assert.strictEqual(Users.isIdInDB(22), false);
+    expect(Users.isIdInDB(22)).toBe(false);
   });
   it('null type', function () {
-    assert.strictEqual(Users.isIdInDB(null), false);
+    expect(Users.isIdInDB(null)).toBe(false);
   });
   it('undefined type', function () {
-    assert.strictEqual(Users.isIdInDB(undefined), false);
+    expect(Users.isIdInDB(undefined)).toBe(false);
   });
 });
 
+describe('get getByIdPw', function () {
+  it('find existed user', function () {
+    const user = {
+      id: 'auddn6676',
+      pw: '12345'
+    }
+    expect(Users.getByIdPw(user)).toMatchObject(user);
+  })
+  it('find not existed user', function () {
+    const user = {
+      id: 'auddn667',
+      pw: '12345'
+    }
+    expect(Users.getByIdPw(user)).toBeUndefined();
+  })
+  it('user is empty object', function () {
+    const user = {};
+    expect(Users.getByIdPw(user)).toBeUndefined();
+  })
+})
+
+describe('test login request', () => {
+  it('should success login', async () => {
+    const user = {
+      id: 'auddn6676',
+      pw: '12345'
+    }
+    const res = await request(app)
+      .post('/users/login')
+      .send(user)
+    expect(res.statusCode).toEqual(200)
+  });
+  it('should not success login', async () => {
+    const user = {
+      id: 'auddn667',
+      pw: '12345'
+    }
+    const res = await request(app)
+      .post('/users/login')
+      .send(user)
+    expect(res.statusCode).toEqual(404)
+  });
+  it("don't have id & pw property", async () => {
+    const user = {};
+    const res = await request(app)
+      .post('/users/login')
+      .send(user)
+    expect(res.statusCode).toEqual(404)
+  });
+  it("param is undefined", async () => {
+    const user = undefined;
+    const res = await request(app)
+      .post('/users/login')
+      .send(user)
+    expect(res.statusCode).toEqual(404)
+  });
+  it("param is null", async () => {
+    const user = null;
+    const res = await request(app)
+      .post('/users/login')
+      .send(user)
+    expect(res.statusCode).toEqual(404)
+  });
+})
