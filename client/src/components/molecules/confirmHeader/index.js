93c971a6658120b1d41404a50aba96922be8e982
zoomkoding <zoomkoding@gmail.com>
2020-07-09 14:15:54 +0900

    feature: 회원가입 완료 페이지 생성

---
+++ client/src/components/molecules/confirmHeader/index.js
@@ -0,0 +1,11 @@
+import { div } from '/utils/elements.js';
+import Img from '/components/atoms/Img/index.js';
+
+const ConfirmHeader = () => div(
+    { className : 'molecules__confirmHeader', },
+    Img('https://ceo.baemin.com/lockpath/images/logo-ceo.png', '배민사장님광장', 274, 40),
+    '회원이 되셨습니다.',
+)
+
+export default ConfirmHeader;
+
