8546284df159efcf6f378f2d0fd2d531b9be3cd2
zoomkoding <gur5381@gmail.com>
2020-07-10 02:38:05 +0900

    etc: a tag 텍스트 바꿈

--- client/src/components/molecules/confirmHeader/index.js
+++ client/src/components/molecules/confirmHeader/index.js
@@ -5,7 +5,7 @@ import A from '/components/atoms/a/index.js';
 const ConfirmHeader = () => div(
     { className : 'molecule-confirmHeader', },
     Img('https://ceo.baemin.com/lockpath/images/logo-ceo.png', '배민사장님광장', 274, 40),
-    div({ className: 'aDiv' }, A('atom-a', '메인으로', '/login')),
+    div({ className: 'aDiv' }, A('atom-a', '로그인하기', '/login')),
 )
 
 export default ConfirmHeader;
