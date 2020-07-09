8141b263156416f3eabaf2334ec5a35cb50b6bef
zoomkoding <gur5381@gmail.com>
2020-07-10 02:36:42 +0900

    feature: email 붙이기

--- server/controllers/user_controller.js
+++ server/controllers/user_controller.js
@@ -4,6 +4,7 @@ const Users = require('../models/users');
 exports.createUser = async (req, res) => {
     const input = req.body;
     input.pw = await bcrypt.hash(input.pw, 10);
+    if(input.emailFront && input.emailRear) input.email = `${input.emailFront}@${input.emailRear}`;
     const user = Users.create(input);
     const { id, name, email, phoneNo } = user;
     res.render('confirm', { user : { id, name, email, phoneNo } });
