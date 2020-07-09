769170324f40ea46caa6d4aa822daa716ef99b25
zoomkoding <zoomkoding@gmail.com>
2020-07-09 15:37:35 +0900

    update: template에 따라 각 page position 정의하도록 하고 class명을 통일했습니다.

--- client/src/components/molecules/loginForm/index.js
+++ client/src/components/molecules/loginForm/index.js
@@ -6,10 +6,10 @@ import Img from '/components/atoms/img/index.js';
 const LoginForm = () => form(
     { className : 'molecule-loginForm', action: 'users/login', method: 'post' },
     Img('https://ceo.baemin.com/lockpath/images/logo-ceo.png', '배민사장님광장', 274, 40),
-    Input('atom-input-full', 'id', '아이디', 'text'),
-    Input('atom-input-full', 'pw', '비밀번호', 'password'),
-    Button('atom-button-full', '로그인'),
-)
+    Input('atom.input.full', 'username', '아이디', 'text'),
+    Input('atom.input.full', 'password', '비밀번호', 'password'),
+    Button('atom.button.full', '로그인'),
+);
 
 export default LoginForm;
 
--- client/src/components/pages/login.js
+++
@@ -1,4 +0,0 @@
-import LoginForm from '/components/molecules/loginForm/index.js';
-
-document.body.appendChild(LoginForm());
-
